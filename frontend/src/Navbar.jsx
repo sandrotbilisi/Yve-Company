93628b2e61b6a0a8e173fc944e6d691344e74aed
TemovsTusishvili <temotusishvili1@gmail.com>
2023-03-30 18:06:45 +0400

    fixed nav err

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -5,30 +5,39 @@ export default function Navbar() {
   <>
    
      
-   <div class="smt">
-        
-        <video controls="" autoplay="y" name="media"><source src="./vid 1.webm  "></video> 
-       <nav>
-        
-           <span class="yve">ივერსი</span>
-            <ul>
-                
-                <li><a href="#">მაღაზია</a></li>
-                <li><a href="#">დახმარება</a></li>
-                <li><a href="#">about us</a></li>
-                
-                    <li><a href="" class="logos"><i class="fa-solid fa-user"></i></a></li>
-                    <li><a href="" class="logos"><i class="fa-solid fa-magnifying-glass"></a></i></li>
-                    
-                
-            </ul>
-        </nav>
-        <div class="content">
-            <h1>თქვენი მეგზური ციფრულ სამყაროში</h1>
-            <a href="#">კატალოგი</a>
-        </div>
-
-    </div>
+   <div className="smt">
+  <video controls="" autoPlay="y" name="media">
+    <source src="./vid 1.webm  " />
+  </video>
+  <nav>
+    <span className="yve">ივერსი</span>
+    <ul>
+      <li>
+        <a href="#">მაღაზია</a>
+      </li>
+      <li>
+        <a href="#">დახმარება</a>
+      </li>
+      <li>
+        <a href="#">about us</a>
+      </li>
+      <li>
+        <a href="" className="logos">
+          <i className="fa-solid fa-user" />
+        </a>
+      </li>
+      <li>
+        <a href="" className="logos">
+          <i className="fa-solid fa-magnifying-glass" />
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <div className="content">
+    <h1>თქვენი მეგზური ციფრულ სამყაროში</h1>
+    <a href="#">კატალოგი</a>
+  </div>
+</div>
 
     </>
   )
