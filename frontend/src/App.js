659ae8e1642c5625a40f37d38ef230ea6a0467ad
TemovsTusishvili <temotusishvili1@gmail.com>
2023-03-30 17:57:22 +0400

    added home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import Navbar from './Navbar';
+import Home  from './Home'; 
 
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
+      
     </>
   )
 }
+
+
 
 export default App;
