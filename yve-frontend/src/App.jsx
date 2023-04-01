700a255aec71ede6a10e1301e30634a4ed5442cc
System Administrator <you@example.com>
2023-04-01 14:47:49 +0400

    Added Explore

--- yve-frontend/src/App.jsx
+++ yve-frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Explore from './components/Explore'
 
 function App() {
   return(
     <>
       <Navbar />
       <Home />
+      <Explore />
     </>
   )
 }
